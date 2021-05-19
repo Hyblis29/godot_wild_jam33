9f7d9babb83b67354fc78b44f449ec0140cb98fd
Hyblis29 <54952850+Hyblis29@users.noreply.github.com>
2021-05-19 13:51:20 +0200

    show player score in Room scene

--- scenes/Room.gd
+++ scenes/Room.gd
@@ -8,7 +8,10 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	var player_score = get_node("/root/Global").player_score
+	$ProgressBar.value = 100 - player_score
+	$ProgressBar2.value = player_score
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -18,9 +21,9 @@ func _ready():
 
 func _on_Phone_pressed():
 	get_tree().change_scene("res://scenes/Phone.tscn")
-	pass # Replace with function body.
+	pass
 
 
 func _on_Computer_pressed():
 	get_tree().change_scene("res://scenes/Computer.tscn")
-	pass # Replace with function body.
+	pass
