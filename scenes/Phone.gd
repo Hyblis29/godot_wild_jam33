b83ee2200e09da6078c028571c266b23d19011e9
Hyblis29 <54952850+Hyblis29@users.noreply.github.com>
2021-05-23 13:52:34 +0200

    correction

--- scenes/Phone.gd
+++ scenes/Phone.gd
@@ -1,21 +1,26 @@
 extends Control
 
 
+# Variables
+var end
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var end = get_node("/root/Global").end()
+	end = get_node("/root/Global").end()
 	if end == null:
 		$RichTextLabel.text = get_node("/root/Global").get_phone_datas()
-		$SendButton.show()
 	else:
 		$RichTextLabel.text = end
-		$SendButton.hide()
 	pass
 
 
 func _on_SendButton_pressed():
-	# next, the player must see the computer
-	get_node("/root/Global").phone = false
-	
-	get_tree().change_scene("res://scenes/Room.tscn")
+	if end == null:
+		# next, the player must see the computer
+		get_node("/root/Global").phone = false
+		get_tree().change_scene("res://scenes/Room.tscn")
+	else:
+		get_node("/root/Global").init()
+		get_tree().change_scene("res://scenes/MainMenu.tscn")
 	pass
